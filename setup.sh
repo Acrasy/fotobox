b5de87dcf1600c0fe30fc504aaa1acd769093ba9
Acrasy <sai.whocares@gmail.com>
2018-05-27 21:20:04 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+##check for privileges
 if [[ $EUID -ne 0 ]]; then
 	echo "This script must be run as root"
 	exit
 fi
 
+##create array with apps to remove
 declare -a toRemove=(
 'idle*'
 'scratch'
@@ -21,15 +23,26 @@ declare -a toRemove=(
 'vlc'
 )
 
+##remove apps in array -> debloating
 for i in "${toRemove[@]}"; do
  apt-get -y purge --auto-remove $i
 done
 
-##needed for printer driver 
+##needed packages for printer driver 
 sudo apt install gcc libtool libssl-dev libc-dev \
 libjpeg-turbo8-dev libpng12-dev libtiff5-dev cups
 
+##install printer driver
+mkdir ~/gutenprint
+wget -P ~/gutenprint https://sourceforge.net/projects/gimp-print/files/gutenprint-5.2/5.2.14/gutenprint-5.2.14.tar.bz2
+tar xjvf gutenprint-5.2.14.tar.bz2 -C ~/gutenprint
+~/gutenprint/configure
+cd ~/gutenprint
+make clean
+make
+sudo make install
 
+#download and extract photobooth software
 echo mkdir ~/PhoyoLinux
 echo cd ~/PhoyoLinux
 echo wget -P ~/PhoyoLinux https://www.phoyosystem.com/custom/binaries/PhoyoLinux.tar.gz
